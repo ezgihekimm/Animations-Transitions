ae8b51514f2f67c72f93af6f4de081059ae8cf26
ezgihekimm <84326561+ezgihekimm@users.noreply.github.com>
2022-01-27 02:16:46 +0300

    commit3

--- src/components/Scroll.vue
+++ src/components/Scroll.vue
@@ -1,29 +1,46 @@
 <template>
+
     <div class="sticky-container">
-        <div class="sticky">
-            <div id="animation-container"></div>
+        <div class="container-fluid">
+            <div class="main_header">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-12 col-lg-12 align-center">
+
+                        <h5 class="mbr-section-title mb-5 mt-o mbr-fonts-style ls-1 display-2 "
+                            style="font-family: 'Roboto', sans-serif; color:#C4E5F6">
+                            3D APPEARANCE</h5>
+
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <div class="sticky d-flex justify-content-end">
+            <div id="animation-container">
+
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
-
-export default {
-}
-
+    export default {}
 </script>
 
 <style>
-.sticky-container {
-    background-color: #121212;
-    height: 1800px;
-}
-.sticky {
-    position: sticky;
-    top: 50px;
-    text-align: center;
-}
-#animation-container {
-    height: 844px;
-}
+    .sticky-container {
+        background-color: #121212;
+        height: 1800px;
+    }
+
+    .sticky {
+        position: sticky;
+        top: 50px;
+        text-align: center;
+    }
+
+    #animation-container {
+        height: 844px;
+    }
 </style>
